1bdd26c7f8dae302d3bc3c6a07cc87bf04f020b2
Greentaylor27 <7196@holbertonstudents.com>
2024-01-05 14:08:36 -0800

    Adding conditionals

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,7 @@
 for tens in range(9):
     for ones in range(tens, 10):
             if tens != ones:
-                print("{}{}".format(tens, ones), end=", ")
+                if tens == 8:
+                    print("{}{}".format(tens, ones), end="")
+                else:
+                    print("{}{}".format(tens, ones), end=", ")
