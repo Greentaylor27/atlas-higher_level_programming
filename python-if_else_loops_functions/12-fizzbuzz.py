209e6f6238aaf5cec54b53f1e078be34e101998f
Greentaylor27 <7196@holbertonstudents.com>
2024-01-05 15:24:14 -0800

    Changing while to if

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def fizzbuzz():
     number = range(1,100)
-    while number <= 100:
+    if number <= 100:
         print(f"{number:d}", end="")
-        #if number % 5 == 0:
-            #print("{Buzz}")
-        #elif number % 3 == 0:
-         #   print("{Fizz}")
-       # elif number % 5 == 0 and number % 3 == 0:
-        #    print("{FizzBuzz}")
