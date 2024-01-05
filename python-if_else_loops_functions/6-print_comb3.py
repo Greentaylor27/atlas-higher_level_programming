efa49ab6d53b652089f17c5bfc3fe5e0427d4ee9
Greentaylor27 <7196@holbertonstudents.com>
2024-01-05 14:15:05 -0800

    Fixing output error

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for tens in range(9):
     for ones in range(tens, 10):
         if tens != ones:
             if tens == 8:
-                print("{}{}".format(tens, ones), end=" ")
+                print("{}{}".format(tens, ones))
             else:
                 print("{}{}".format(tens, ones), end=", ")
