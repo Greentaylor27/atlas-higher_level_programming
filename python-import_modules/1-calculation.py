9d3161dded5c581a75413188ec1e9d248669989f
Greentaylor27 <7196@holbertonstudents.com>
2024-01-06 14:40:40 -0800

    Removing conditionals

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ from calculator_1 import add, sub, mul,div
 a = 10
 b = 5
 if __name__ == "__main__":
-    if add(a, b):
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sub(a, b):
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif mul(a, b):
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    else:
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
