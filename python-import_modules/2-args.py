84bfac395664fae270b29f9caa85fb1f2bd8ef4a
Greentaylor27 <7196@holbertonstudents.com>
2024-01-06 15:15:10 -0800

    Struggle bus is REAL

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    la = len(argv
-    print("{}".format(la))
+    la = len(sys.argv) - 1
+    if la == 0:
+        print("0 arguements.")
+    elif la == 1:
+        print("1 argument:")
+    elif la > 1:
+        print("{} arguments:".format(la))
+
+    for i in range(1, la + 1):
+        print("{}: {}".format(la, sys.argv[i]))
+    
