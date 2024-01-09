096ac00acb62c1fd2f4ecbf06d99c2e30141d0c1
Greentaylor27 <7196@holbertonstudents.com>
2024-01-09 10:29:04 -0800

    Here we go again

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newlist = []
-    if my_list is None or element is None:
-        return None
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    for i in range(len(my_list)):
-        if i == idx:
-            newlist.append(element)
-        newlist.append(my_list[i])
-    return newlist
+    new_list = list(my_list)[:idx] + [element] + list(my_list)[idx + 1:]
+    return new_list
