96df105486146506b7206b71c592471a0e0ea50d
Greentaylor27 <7196@holbertonstudents.com>
2024-01-09 08:25:07 -0800

    Maybe more?

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,7 @@
 def new_in_list(my_list, idx, element):
     if my_list is None or element is None:
         return None
-    my_list.insert(idx, element)
-    return my_list
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    newlist = my_list.insert(idx, element)
+    return newlist
