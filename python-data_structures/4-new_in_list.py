c23ee6942ea5851a82343f70693d3891d2fcb39d
Greentaylor27 <7196@holbertonstudents.com>
2024-01-09 08:31:50 -0800

    Replacing .insert()

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    newlist = []
     if my_list is None or element is None:
         return None
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    newlist = my_list.insert(idx, element)
+    for i in range(len(my_list)):
+        if i == idx:
+            newlist.append(element)
+        newlist.append(my_list[i])
     return newlist
