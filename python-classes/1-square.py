dca408293b398485e0069974a550c0a758978724
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 12:18:42 -0600

    Adding new documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
-    """Attributes of an object called Square"""
-    __size = None
+    def __init__(self, size):
+        """Attributes of an object called Square"""
+        self.__size = size
     
