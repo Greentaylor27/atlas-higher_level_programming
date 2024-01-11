77529e11ee3477866a3a353b3c682aa5d14f4a97
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 15:21:23 -0600

    Fixing Pycodestyle error

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -28,7 +28,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def my_print(self):
         """Prints a literal square using #"""
         if self.__size == 0:
