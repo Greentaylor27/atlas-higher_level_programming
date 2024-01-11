047a3a410fc0d102e1ff2370dce7006e3cc0ad5b
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 14:48:26 -0600

    Fixing more pycodestyle

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,15 +12,15 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+    
     def area(self):
         """Method for finding the area of this class"""
         return (self.__size * self.__size)
-    
+
     @property
     def size(self):
         return self.__size
-     
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
