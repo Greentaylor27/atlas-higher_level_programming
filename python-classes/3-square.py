d1c2db0689e894f22671863e498f02fc8bc471e5
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 13:33:08 -0600

    Clearing up pycodestyle errors

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,13 +7,11 @@ class Square:
     def __init__(self, size=0):
         """Initailizes Attributes"""
         self.__size = size
-        
         """Conditionals within the class"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
     def area(self):
         """Method for finding the Area"""
         return (self.__size * self.__size)
