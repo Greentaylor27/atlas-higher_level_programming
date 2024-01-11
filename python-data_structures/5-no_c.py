dde7fe0a983aac90cceb6f4bbc8302288c4a96c8
unknown <7196@holbertonstudents.com>
2024-01-11 11:02:50 -0600

    Test

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string is None:
-        return None
-    
+    char = 'c', 'C'
+    my_string = ''.join([char for char in my_string if char not in ('c', 'C')])
+    return my_string
