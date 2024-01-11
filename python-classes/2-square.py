84a4323b227a5e7eec0ed1a37d909e7b2a8cc8a7
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 12:44:12 -0600

    Trying something

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,11 +4,11 @@
 
 class Square:
     """Attributes of an object know as Square"""
-    def __init__(self, size=None):
+    def __init__(self, size):
+        """Initalizes Attributes"""
+        self.__size = size
         """Conditionals within the class"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        """Initalizes Attributes"""
-        self.__size = size
