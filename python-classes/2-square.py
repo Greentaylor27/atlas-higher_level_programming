352715e9af172bceb54ab22a0695358feb268288
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 12:38:43 -0600

    Fixing grammar

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size):
         """Conditionals within the class"""
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         """Initalizes Attributes"""
         self.__size = size
