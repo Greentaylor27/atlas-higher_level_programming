d07143f528c18822f31af35423276b7af01e9c62
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 13:00:52 -0600

    First commit

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class called Square"""
+
+
+class Square:
+    """Attributes of an object known as Square"""
+    def __init__(self, size=0):
+        """Initailizes Attributes"""
+        self.__size = size
+        
+        """Conditionals within the class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size > 0:
+            raise ValueError("size must be >= 0")
+        
+        """Method for finding the sqrt"""
+        def area(self):
+            return size * size
