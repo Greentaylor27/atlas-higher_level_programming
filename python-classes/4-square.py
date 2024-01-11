3b9b85812a853cb703652d9a25ecdb373059b3d1
Greentaylor27 <7196@holbertonstudents.com>
2024-01-11 14:41:43 -0600

    Changing Size to Value

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -23,8 +23,8 @@ class Square:
     
     @size.setter
     def size(self, value):
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        value = self.__size
+        self.__size = value
