f3eaac0e0ab258fbf809bd3e3e849c06c189aa30
Greentaylor27 <7196@holbertonstudents.com>
2024-01-15 09:39:16 -0600

    Shortening code

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,18 +1,12 @@
 #Function that adds two integers or floats together and returns sum
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    """Conditionals for the function to make sure all inputs are ints/floats"""
-    if isinstance(a, float):
-        a = int(a)
-    elif isinstance(a, int):
-        a = a
-    else:
+    """
+    Conditionals for the function to make sure 
+    all inputs are ints/floats"""
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, float):
-        b = int(b)
-    elif isinstance(b, int):
-        b = b
-    else:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     """If the function passes then it should return the sum"""
     return (int(a + b))
