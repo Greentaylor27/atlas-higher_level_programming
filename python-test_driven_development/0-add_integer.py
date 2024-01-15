196024f84e4b0c9ed4d51945e70ed7df226dcebc
Greentaylor27 <7196@holbertonstudents.com>
2024-01-15 09:46:06 -0600

    Adding docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,28 @@
-#Function that adds two integers or floats together and returns sum
+"""
+Function that adds two integers or floats together and returns sum
+"""
 #!/usr/bin/python3
 def add_integer(a, b=98):
     """
-    Conditionals for the function to make sure 
-    all inputs are ints/floats"""
+    Adds two numbers together and returns the Sum.
+    Both numbers must be integers or float
+    
+    Args:
+        a (int, float): first int/float to be used
+        b (int, float): second int/float to be used
+
+    Raises:
+        TypeError: a is not an integer
+        TypeError: b is not an integer
+
+    Returns:
+        int: Sum of two floats or ints
+    """
+    #Conditionals for the function to make sure 
+    #all inputs are ints/float
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    """If the function passes then it should return the sum"""
+    #If the function passes then it should return the sum
     return (int(a + b))
