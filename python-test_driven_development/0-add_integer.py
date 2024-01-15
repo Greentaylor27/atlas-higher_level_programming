5e8da1668d92694f808bd777e4ebbcdecff6ae99
Greentaylor27 <7196@holbertonstudents.com>
2024-01-15 08:34:16 -0600

    Checking function for correct output

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if isinstance(a, float):
+        a = int(a)
+    elif isinstance(a, int):
+        a = a
+    else:
+        raise TypeError("a must be an integer")
+    if isinstance(b, float):
+        b = int(b)
+    elif isinstance(b, int):
+        b = b
+    else:
+        raise TypeError("b must be an integer")
+    return(int(a + b))
+    
