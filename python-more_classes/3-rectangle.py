9909bce5e264ced1fde5540db7db0bcdcb04c7b0
Greentaylor27 <7196@holbertonstudents.com>
2024-01-16 16:59:52 -0600

    Readding empty print

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -66,6 +66,7 @@ class Rectangle:
             for x in range(self.__height):
                 for y in range(self.__width):
                     print("#", end="")
+                print("")
                 
     def __str__(self):
         """String representation of a rectangle"""
