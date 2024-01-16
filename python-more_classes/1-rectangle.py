df6bb04560403ef2daaf5ffcfbdc5b6b54877543
Greentaylor27 <7196@holbertonstudents.com>
2024-01-16 16:28:07 -0600

    Reformatted

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -19,30 +19,30 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = height
         
-        @property
-        def width(self):
-            """Getter for width"""
-            return self.__width
+    @property
+    def width(self):
+        """Getter for width"""
+        return self.__width
         
-        @width.setter
-        def width(self, value):
-            """Setter for width"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
             
-        @property
-        def height(self):
-            """Getter for height"""
-            return self.__height
+    @property
+    def height(self):
+        """Getter for height"""
+        return self.__height
         
-        @height.setter
-        def height(self, value):
-            """Height setter"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
