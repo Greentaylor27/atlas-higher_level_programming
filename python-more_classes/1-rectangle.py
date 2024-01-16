53bd5a934401749cf74d97c3419004da1df00d38
Greentaylor27 <7196@holbertonstudents.com>
2024-01-16 16:19:55 -0600

    Fixing isinstance in setter

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
         @width.setter
         def width(self, value):
             """Setter for width"""
-            if not isinstance(width, int):
+            if not isinstance(value, int):
                 raise TypeError("width must be an integer")
             if width < 0:
                 raise ValueError("width must be >= 0")
@@ -41,7 +41,7 @@ class Rectangle:
         @height.setter
         def height(self, value):
             """Height setter"""
-            if not isinstance(height, int):
+            if not isinstance(value, int):
                 raise TypeError("height must be an integer")
             if height < 0:
                 raise ValueError("height must be >= 0")
