9a699245eaccb493a1690c72c9740362325b5bbb
Greentaylor27 <7196@holbertonstudents.com>
2024-01-16 09:00:11 -0600

    Function file should be complete

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,9 +5,37 @@ Function that divides a matrix
 
 
 def matrix_divided(matrix, div):
-    if not isinstance(matrix, (int, float)):
+    """
+    Function that divides an index in a matrix
+
+    Args:
+        matrix (list): List of ints or floats
+        div (int, float): divisor
+
+    Raises:
+        TypeError: matrix must be a matrix (list of lists) of integers/floats
+        TypeError: Each row of the matrix must have the same size
+        TypeError: div must be a number
+        ZeroDivisonError: divison by zero
+
+    Returns:
+        list: A matrix with new elements 
+    """
+    # Conditionals for error raising
+    result = []
+    if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if not all(len(row) == (matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (float, int)):
+        raise TypeError("div must be a number")
     if div == 0:
-        
-    return (matrix / div)
+        raise ZeroDivisionError("division by zero")
+    # Actual function
+    for row in matrix:
+        new_row = []
+        for element in row:
+            new_element = float(element / div)
+            new_row.append(float(f'{new_element:.2f}'))
+        result.append(new_row)
+    return (result)
