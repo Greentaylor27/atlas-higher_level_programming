f2799ca63a7ea33df3c86b7daa9ea974e39d8cf9
Greentaylor27 <7196@holbertonstudents.com>
2024-01-16 14:02:29 -0600

    Why did this happen to me...

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -22,7 +22,7 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
-    
+
     # Meat and potatoes of the code
     if size == 0:
         pass
