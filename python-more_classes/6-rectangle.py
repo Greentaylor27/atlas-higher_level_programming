17c0da5bc07de441a6ebcf18d8b9c2856f6febea
Greentaylor27 <7196@holbertonstudents.com>
2024-01-17 16:36:44 -0600

    Fixing pycodestyle error

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     Attributes for Rectangle
     """
     number_of_instances = 0
+    
     def __init__(self, width=0, height=0):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
@@ -19,7 +20,7 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-        #Increment the class attribute when a new instance is created
+        # Increment the class attribute when a new instance is created
         Rectangle.number_of_instances += 1
 
     @property
@@ -77,4 +78,4 @@ class Rectangle:
         """Prints a message when the rectangle is being destroyed."""
         print("Bye rectangle...")
         # Decrement the class attribute when an instance is deleted
-        Rectangle.number_of_instances -=1
+        Rectangle.number_of_instances -= 1
