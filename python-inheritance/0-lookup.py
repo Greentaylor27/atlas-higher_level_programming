59c38bac283a89c0a461db9d74c6fb03bcaed5a3
Greentaylor27 <7196@holbertonstudents.com>
2024-01-22 16:23:56 -0600

    adding two blank lines after docstring

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Returns the available list of attributes and methods of an obj"""
+
+
 def lookup(obj):
     """
     Function that returns the list of attributes and methods of an obj
