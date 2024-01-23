b0dbb4890fa795745b6cca945db8449942732ac1
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 14:46:23 -0600

    Getting something in to see what I need for checker

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class known as MyList"""
+
+
+class MyList:
+    """Child class for what I assume is list"""
+
+
+    def print_sorted(self):
+        """Method that should sort list"""
+        SortedList = sorted(self)
+        print(SortedList)
