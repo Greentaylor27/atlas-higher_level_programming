37915a45ee7dc40368c18907267d68c5a6a3c35c
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 17:47:07 -0600

    Documenting function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,13 @@ Module that opens and reads a file
 
 
 def read_file(filename=""):
+    """
+    Function that opens and reads a file
+
+    Args:
+        filename (str, optional): File name of the file you want open and read.\
+            Defaults to "".
+    """
     x = filename
     with open(x) as file:
         print(file.read())
