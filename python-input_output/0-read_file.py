07c248a12600cd104fb03880722e9366cbfa25c9
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 17:50:35 -0600

    Fixing PEP8 issues

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,8 +9,8 @@ def read_file(filename=""):
     Function that opens and reads a file
 
     Args:
-        filename (str, optional): File name of the file you want open and read.\
-            Defaults to "".
+        filename (str, optional): File name of the file you want\
+            open and read. Defaults to "".
     """
     x = filename
     with open(x) as file:
