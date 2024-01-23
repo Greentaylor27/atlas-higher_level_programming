d0b39482b02c9373f28bba54ecda738fdfd9eaaf
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 16:12:28 -0600

    Hope this is all they are looking for

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class for BaseGeometry"""
+
+
+class BaseGeometry:
+    """Methods for BaseGeometry"""
+    def __init__(self):
+        pass
+    def area(self):
+        try:
+            pass
+        except:
+            raise Exception("area() if not implemented")
