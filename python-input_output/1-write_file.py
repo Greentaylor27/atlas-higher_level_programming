4f9fcdf87e6ac679438213d9f4cf916c702644d5
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 17:59:01 -0600

    First push

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module that creates a file if it doesn't exsist\
+Appends a file if it has already been created
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function that overwrites a file if it already exsists\
+    or creates a new file if there is none
+
+    Args:
+        filename (str, optional): File that you are tying to open.\
+            Defaults to "".
+        text (str, optional): What you want written down. Defaults to "".
+    """
+    x = filename
+    with open(x, "w") as file:
+        return file.write(text)
+        
