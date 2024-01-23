7a383798e3fb6c520087c977e224490bf6b45425
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 17:21:12 -0600

    Adding super() to code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -21,5 +21,8 @@ class BaseGeometry:
 class Rectangle(BaseGeometry):
     """Subclass of A rectangle"""
     def __init__(self, width, height):
-        self.__width = width
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__height = height
+        self.__width = width
