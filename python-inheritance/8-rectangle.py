62987e8972d64d561096f0071e49caad7163b7a5
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 17:34:58 -0600

    Reformatting after reading task again

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,27 +2,24 @@
 """class for BaseGeometry"""
 
 
-class BaseGeometry:
-    """Methods for BaseGeometry"""
-    def __init__(self):
-            pass
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        self.name = name
-        self.value = value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """Subclass of A rectangle"""
+    """
+    Class to define A rectangle
+
+    inhertiance: BaseGeometry
+
+    Args:
+        BaseGeometry (Class): Parent class
+
+    Attributes:
+        height (int): Height of rectangle
+        width (int): Width of rectangle
+    """
     def __init__(self, width, height):
         super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-        self.__height = height
         self.__width = width
+        self.__height = height
