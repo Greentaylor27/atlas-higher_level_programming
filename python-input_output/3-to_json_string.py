f1208a3c2e93b376ac2e646007abfcd7c3ce4cd4
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 18:19:37 -0600

    First commit of JSON to string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#1/usr/bin/python3
+"""
+Module that returns the JSON representation of an onject
+"""
+import json
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object
+
+    Args:
+        my_obj (object): Object you want the JSON representation of
+    """
+    return json.dumps(my_obj)
