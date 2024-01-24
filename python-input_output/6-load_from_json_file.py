7283a021c95bd712c5c7799ba7f302b00e996afe
Greentaylor27 <7196@holbertonstudents.com>
2024-01-23 20:47:20 -0600

    First commit of 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that loads from a JSON to txt file
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that loads a JSON file to text
+
+    Args:
+        filename (string): file you are trying to open
+    """
+    with open(filename) as x:
+        return json.loads(x)
