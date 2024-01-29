c650a4ee64bf7c5a219f76e495ad59bb03df24cf
Greentaylor27 <7196@holbertonstudents.com>
2024-01-29 13:21:55 -0600

    Got width and height backwards on display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,8 +106,8 @@ class Rectangle(Base):
         """
         for x in range(self.__y):
             print()
-        for y in range(self.__width):
-            print(" " * self.__x + "#" * self.__height)
+        for y in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         i = self.id
