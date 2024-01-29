41c602ae59a36f70504817dfca0d3b2be9ee3695
Greentaylor27 <7196@holbertonstudents.com>
2024-01-29 11:55:45 -0600

    Fixing documention

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,32 +13,40 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
-    """Getter and setters for attributes"""
 
+    """Getter and setters for attributes"""
+    @property
     def get_width(self):
         """Getter for width"""
         return self.__width
+    @width.setter
     def set_width(self, value):
         """Setter for width"""
         self.__width = value
 
+    @property
     def get_height(self):
         """Getter for height"""
         return self.__height
+    @height.setter
     def set_height(self, value):
         """Setter for height"""
         self.__height = value
 
+    @property
     def get_x(self):
         """Getter for x"""
         return self.__x
+    @x.setter
     def set_x(self, value):
         """Setter for x"""
         self.__x = value
 
+    @property
     def get_y(self):
         """Getter for y"""
         return self.__y
+    @y.setter
     def set_y(self, value):
         """Setter for y"""
         self.__y = value
