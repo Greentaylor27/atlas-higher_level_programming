9996d402676ef8a95008e796e38d534a109c3f23
Greentaylor27 <7196@holbertonstudents.com>
2024-01-30 13:25:25 -0600

    Figuring out with @staticmethod goes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,11 +18,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         Method that returns the JSON representation of list_dictionaries
         """
-        @staticmethod
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
