4b8e867c33919f5a71639f5581c6b9067e500815
Greentaylor27 <7196@holbertonstudents.com>
2024-01-30 13:09:06 -0600

    Fixing PEP8 style errors

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -136,15 +136,15 @@ class Rectangle(Base):
 
         for key, value in kwargs.items():
             setattr(self, key, value)
-            
+
     def to_dictionary(self):
         """
         Method that returns the dictionary representation of a Rectangle
         """
         return {
-            'id' : self.id,
-            'width' : self.__width,
-            'height' : self.__height,
-            'x' : self.__x,
-            'y' : self.__y
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
             }
