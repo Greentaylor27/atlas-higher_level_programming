8af33af81fe1eb312912cdb47f5b396c2509376e
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 14:39:35 -0600

    One day I'll spell exists without messing up

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Creates a new table and inserts records into them
-CREATE TABLE IF NOT EXIST second_table(
+CREATE TABLE IF NOT EXISTS second_table(
     id INT,
     name VARCHAR(256),
     score INT
