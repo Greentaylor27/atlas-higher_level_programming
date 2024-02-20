4c7baa18686b47dcc7f68115c8a771daf774f270
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 17:18:27 -0600

    Adding white space

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates a database and table for states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
-    id INT NOT NULL DEFAULT 1 UNIQUE AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
     name VARCHAR(256),
     PRIMARY KEY (id)
 );
