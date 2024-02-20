a8c25bb631b346c5133840db9e4515ead9bb7b89
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 14:01:47 -0600

    Making first table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,11 +1,5 @@
 -- Creates a new table in current database
-IF NOT EXISTS first_table
-BEGIN
-    CREATE TABLES first_table(
-        id int
-        name VARCHAR(256)
-    )
-END
-ELSE
-    PRINT 'TABLE EXISTS'
-END
+CREATE TABLE if NOT EXISTS first_table (
+    id int
+    name VARCHAR(256)
+)
