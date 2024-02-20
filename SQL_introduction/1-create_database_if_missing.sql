bcc9faac0d7ae2e179218ed27620f2c7363f0fa0
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 13:21:55 -0600

    Over thinking again

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,2 @@
 -- Creates a database if it doesn't exsist
-IF EXSIST (SELECT name FROM sys.databases WHERE name = 'hbtn_0c_0')
-BEGIN
-    PRINT "database exsits"
-END
-ELSE
-BEGIN
-    CREATE DATABASE 'hbtn_0c_0'
-END
+CREATE DATABASE IF NOT EXSIST 'hbtn_0c_0'
