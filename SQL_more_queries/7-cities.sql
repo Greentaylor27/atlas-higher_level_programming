ea2fda4b84d7b60f40f4cc13da7b36d3ead024ea
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 17:33:43 -0600

    Uncertain about this one

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,9 +1,9 @@
 -- Creates a table (Cities) to the database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT UNIQUE,
     state_id INT NOT NULL,
     name VARCHAR(256),
     PRIMARY KEY (id)
-    FOREIGN KEY (id) REFERENCES state_id
-)
+);
