35092190c10b2ff807bc4edeffca1f69d874191e
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 16:53:52 -0600

    Adding constraint to the table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- creates a new table with a constraint
 CREATE TABLE IF NOT EXISTS forced_name(
     id INT,
-    name VARCHAR(256)
+    name VARCHAR(256) NOT NULL
 );
-ALTER COLUMN name VARCHAR NOT NULL;
+
