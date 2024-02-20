ad1ef8119643b3231368ed82e88e88fb9946655b
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 17:13:18 -0600

    EVEN more Constraints

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates a database and table for states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT NOT NULL DEFAULT 1 UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256),
+    PRIMARY KEY (id)
+)
