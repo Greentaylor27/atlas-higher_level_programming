989161d3af4fc8718d467c367f5eda4ac7e53250
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 15:55:21 -0600

    Removing with grant options

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates a new user for the SQL server
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEDGES ON *.* TO 'user_0d_1'@'localhost';
