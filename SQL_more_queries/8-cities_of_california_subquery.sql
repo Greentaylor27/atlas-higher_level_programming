bdb490fb7151919262d9198b077e0915b4e2bfd3
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 17:50:51 -0600

    Fixing SEVERAL Syntax errors

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Displays all cities within California
-SELECT cities.id cities.name FROM cities, states WHERE cities.state_id = state_id AND states.name = 'California' ORDER BY cities.id ASC;
+SELECT cities.id, cities.name FROM cities, states WHERE cities.state_id = states.id AND states.name = 'California' ORDER BY cities.id ASC;
