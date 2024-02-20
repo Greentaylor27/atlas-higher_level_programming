7a0626cd4ab767655167458f2e380c16a9065cc4
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 17:48:10 -0600

    Ordering by id not name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Displays all cities within California
-SELECT cities.id cities.name FROM cities, states WHERE cities.state_id = state_id AND states.name = 'California' ORDER BY cities.name ASC;
+SELECT cities.id cities.name FROM cities, states WHERE cities.state_id = state_id AND states.name = 'California' ORDER BY cities.id ASC;
