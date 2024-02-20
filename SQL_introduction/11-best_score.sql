99a13927f16c12d8e259b77e5eba6f4fe1c05f4e
Greentaylor27 <7196@holbertonstudents.com>
2024-02-20 14:52:50 -0600

    Adding descending order

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- Shows the Score and name for a score higher than 10
-SELECT score, name FROM second_table WHERE score > 10;
+SELECT score, name FROM second_table WHERE score > 10 ORDER BY score DESC;
