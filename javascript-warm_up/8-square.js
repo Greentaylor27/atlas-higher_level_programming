78b68600e9962f23056ff8e3276f7511b2a9cf96
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 20:03:17 +0000

    Setting string to const

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 let x = process.argv[2];
+const myString = 'x'
 
 if (!isNaN(x)) {
   x = parseInt(x);
@@ -11,7 +12,7 @@ if (x > 0) {
   for (let i = 0; i < x; i++) {
     let row = '';
     for (let j = 0; j < x; j++) {
-      row += 'x';
+      row += myString;
     }
     console.log(row);
   }
