a3c23cf8b1e6b8663fcf12a85f2f49020db45a21
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 17:43:15 +0000

    Removing else if statement

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,8 +4,6 @@ const Arg = process.argv.slice(2);
 
 if (Arg[0] === undefined) {
   console.log('No argument');
-} else if (Arg[0] === Array[1]) {
-  console.log(Arg);
-} else{
-  console.log(Arg.join(` `))
+} else {
+  console.log(Arg.join(' '));
 }
