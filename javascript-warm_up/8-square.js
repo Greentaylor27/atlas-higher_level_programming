64161318906799f1e4120085bf03b687025fc74c
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 20:01:10 +0000

    SQUARE

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+let x = process.argv[2];
+
+if (!isNaN(x)) {
+  x = parseInt(x);
+} else {
+  console.log('Missing number of occurrences');
+}
+if (x > 0) {
+  for (let i = 0; i < x; i++){
+    let row = '';
+    for (let j = 0; j < x; j++){
+      row += 'x'
+    }
+    console.log(row)
+  }
+}
