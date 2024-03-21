e940ac59cecec91366cd8356f5fe1a73ba40a8c6
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 18:45:06 +0000

    Too many x--;

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,12 +4,11 @@ let x = process.argv[2];
 const myString = 'C is fun';
 
 if (!isNaN(x)) {
-  x = parseInt(x - 1);
+  x = parseInt(x);
 } else {
   console.log('Missing number of occurrences');
 }
 if (x > 0) {
-  x--;
   while (x > 0) {
     console.log(myString);
     x--;
