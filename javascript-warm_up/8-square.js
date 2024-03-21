bacfe5d35ed81738504989041fddb30c8500445e
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 20:05:17 +0000

    Fixing else output

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 let x = process.argv[2];
-const myString = 'X'
+const myString = 'X';
 
 if (!isNaN(x)) {
   x = parseInt(x);
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
 if (x > 0) {
   for (let i = 0; i < x; i++) {
