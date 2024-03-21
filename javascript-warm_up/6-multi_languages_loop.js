b493c8c2a3085d06ddf6324d831bb42dc6ce7117
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 18:17:51 +0000

    Defining i

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 const myVar = ['C', 'Python', 'JavaScript'];
 const yourVar = ['fun', 'cool', 'amazing'];
 
-for (i = 0; i < myVar.length; i++) {
+for (const i = 0; i < myVar.length; i++) {
   console.log(`${myVar[i]} is ${yourVar[i]}`);
 }
