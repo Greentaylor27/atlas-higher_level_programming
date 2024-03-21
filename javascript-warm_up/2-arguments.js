11c2fd393e7bfda2bc77282106880a3438700794
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 17:12:15 +0000

    .slice is important

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 
-const [, , Arg] = process.argv;
+const Arg = process.argv.slice(2);
 
 if (Arg.length == 1){
     console.log('Argument found')
 }
-else if (Arg.length > 1){
+else if (Arg.length >= 2){
     console.log('Arguments found')
 }
-else if (Arg.length < 1){
+else if (Arg.length == 0){
     console.log('Argument not found')
 }
 
