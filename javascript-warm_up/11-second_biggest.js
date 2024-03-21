58b40b20ce879c0f13f197ffc643bfb014d4357c
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 21:13:12 +0000

    Sorting

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const Arr = process.argv.slice(2);
+
+function FindSecondLargest (a) {
+  if (a.length < 2) {
+    return 0;
+  }
+
+  let largest = a[0];
+  let secondLargest = -Infinity;
+
+  for (let i = 1; i < a.length; i++) {
+    if (a[i] > largest) {
+      secondLargest = largest;
+      largest = a[i];
+    } else if (a[i] > secondLargest && a[i] !== largest) {
+      secondLargest = a[i];
+    }
+  }
+
+  if (secondLargest === -Infinity) {
+    return 0;
+  } else {
+    return secondLargest;
+  }
+}
+
+console.log(FindSecondLargest(Arr));
