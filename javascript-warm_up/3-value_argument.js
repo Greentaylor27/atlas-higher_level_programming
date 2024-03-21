80af4c377c93f324b7af98acab0332fd2b812eee
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 17:38:38 +0000

    Trying this out

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -4,6 +4,8 @@ const Arg = process.argv.slice(2);
 
 if (Arg[0] === undefined) {
   console.log('No argument');
-} else if (Arg[0] === Array) {
-  console.log(Arg.join(' '));
+} else if (Arg[0] === Array[1]) {
+  console.log(Arg);
+} else{
+  console.log(Arg.join(` `))
 }
