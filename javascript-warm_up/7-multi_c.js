a3c92bdcc95ac02c69335760fa248c0a3993f62f
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 18:35:43 +0000

    Multiple lines

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+let x = process.argv[2];
+const myString = 'C is fun';
+
+if (!isNaN(x)) {
+  x = parseInt(x);
+} else {
+  console.log('Missing number of occurrences');
+}
+if (x > 0) {
+  x--;
+  while (x > 0) {
+    console.log(myString);
+    x--;
+  }
+}
