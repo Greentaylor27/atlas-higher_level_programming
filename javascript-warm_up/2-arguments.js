cf62722fa8992160748e9be7f909672bd8cd9d84
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 17:09:39 +0000

    Learning conditionals

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,13 @@
 
 const [, , Arg] = process.argv;
 
-if (Arg){
+if (Arg.length == 1){
+    console.log('Argument found')
+}
+else if (Arg.length > 1){
     console.log('Arguments found')
 }
-else{
+else if (Arg.length < 1){
     console.log('Argument not found')
 }
 
