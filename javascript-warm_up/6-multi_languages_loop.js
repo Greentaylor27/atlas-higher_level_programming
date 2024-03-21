9a69a33043a2ea178363300d943ec12b39bd2388
Greentaylor27 <7196@holbertonstudents.com>
2024-03-21 18:16:51 +0000

    FOR LOOPS FOR THE WIN

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myVar = ['C', 'Python', 'JavaScript'];
+const yourVar = ['fun', 'cool', 'amazing'];
+
+for (i = 0; i < myVar.length; i++) {
+  console.log(`${myVar[i]} is ${yourVar[i]}`);
+}
