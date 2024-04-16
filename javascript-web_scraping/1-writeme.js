5f7a1b554bad41196c211d9d97037e466cf8beea
Greentaylor27 <greentaylor18@gmail.com>
2024-04-16 18:23:25 +0000

    Web scraping write file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const fs = require('fs')
+const process = require('process')
+const arg = process.argv
+
+if (arg.length < 4) {
+  console.log("Please provide a File name OR str")
+  process.exit(1);
+}
+
+const fileName = arg[2];
+const data = arg[3];
+
+fs.writeFile(filename, data, (err) => {
+  if (err) {
+    console.log(err)
+  }
+});
