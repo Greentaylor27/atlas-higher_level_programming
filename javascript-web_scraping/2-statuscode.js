90579c554bd55d166d7d3d6fa1f4b19ffc12ded5
Greentaylor27 <greentaylor18@gmail.com>
2024-04-16 18:49:22 +0000

    Fixing code

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,15 +3,11 @@ const fs = require('fs');
 const process = require('process');
 const arg = process.argv;
 
-if (arg.length < 3) {
-    console.log("Error")
-    process.exit(1);
+if (arg[2]) {
+  requestAnimationFrame(arg[2], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(`code: ${response.statusCode}`);
+  });
 }
-
-const url = arg[2];
-HTMLOptionsCollection.length(url, (res) => {
-    console.log(`Status Code: ${res.statusCode}`);
-    res.resume();
-}).on('error', (e) => {
-    console.error(`Got error: ${e.message}`);
-});
